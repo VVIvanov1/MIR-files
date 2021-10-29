d19ddd1f98b2b3fc43e2ab2c929670b950a8296f
= <=>
2021-10-29 14:57:08 +0600

    add emd record processing to gsheets and gdrive

--- arj/emd.js
+++ arj/emd.js
@@ -1,42 +1,82 @@
-const fs = require('fs')
-const path = require('path')
-const getHeader = require("../utils/getHeader")
-const parseHeader = require("../utils/parseHeader")
-const mirType = require("../utils/mirType")
+const fs = require("fs");
+const path = require("path");
+const getHeader = require("../utils/getHeader");
+const parseHeader = require("../utils/parseHeader");
+const mirType = require("../utils/mirType");
+const filePath = "C:\\development\\MIR\\files\\AAAJVGALL.MIR";
+// const rawText = fs.readFileSync(filePath, "utf8");
+// const parsed = parseHeader(getHeader(rawText));
+const dateTimeConverter = require("../helpers/dateTimeConverter");
+const [convertDate, convertTime] = dateTimeConverter;
+// const type = mirType(parsed);
+// let emdString = /A29.*\r\nED.*/gm;
+const getDomInt = require("../utils/getDomInt");
+// let emdArrData = rawText.match(emdString);
+// let itinType = getDomInt(parsed);
 
-const filePath = "C:\\development\\MIR\\MIR-files\\files\\AAAJVGAL.MIR"
+function processEmdRec(rout) {
+  const rawText = fs.readFileSync(rout, "utf8");
+  const parsed = parseHeader(getHeader(rawText));
+  const type = mirType(parsed);
+  let itinType = getDomInt(parsed);
+  let emdString = /A29.*\r\nED.*/gm;
+  let emdArrData = rawText.match(emdString);
+  
+  let emds = getEMDrecord(emdArrData);
+  let toReturn = emds.map(em => {
+    let dataOrder = {
+      issueDate: convertDate(parsed.T50DTE),
+      issueTime: convertTime(parsed.T50TME),
+      gds: parsed.T50TRC,
+      pnr: parsed.T50RCL,
+      "MIR type": null,
+      paxTicket: null,
+      paxName: null,
+      flights: null,
+      departure: null,
+      arrival: null,
+      airline: null,
+      paxFare: null,
+      taxes: null,
+      paxTotal: null,
+      paxTaxes: null,
+      exchangeFor: null,
+      bookedAgent: parsed.T50AGS,
+      ticketedAgent: parsed.T50AGT,
+      bookedPCC: parsed.T50BPC,
+      issuesPCC: parsed.T50TPC,
+      "itinerary type": null,
+      processed: false,
+    };
+    let obj = Object.assign(dataOrder, em, type, itinType);
+    return obj
+  });
+  let amdVals = toReturn.map(em => Object.values(em).map(val=>{return val}))
+  return toReturn;
+}
 
-const rawText = fs.readFileSync(filePath, "utf8");
+function getEMDrecord(data) {
+  let emdArr = data.map((row) => {
+    let tktReg = /(?<=RL-).*/g;
+    let forTkt = row.match(tktReg)[0];
+    let emd = {
+      exchangeFor:
+        forTkt.length > 10
+          ? `EMD Оформлен к билету ${
+              forTkt.substring(0, 3) + "-" + forTkt.substring(3)
+            }`
+          : "",
 
-const parsed = parseHeader(getHeader(rawText))
-const type = mirType(parsed)
+      paxName: row.slice(14, 69).trimEnd(),
+      paxTicket:
+        row.slice(69, 82).substring(0, 3) +
+        "-" +
+        row.slice(69, 82).substring(3),
+      paxTotal: Number(row.slice(145, 157).trim()),
+    };
+    return emd;
+  });
+  return emdArr;
+}
 
-
-let dataOrder = {
-    issueDate: null,
-    issueTime: null,
-    gds: null,
-    pnr: null,
-    "MIR type": null,
-    paxTicket: null,
-    paxName: null,
-    flights: null,
-    departure: null,
-    arrival: null,
-    airline: null,
-    paxFare: null,
-    taxes: null,
-    paxTotal: null,
-    paxTaxes: null,
-    exchangeFor: null,
-    bookedAgent: null,
-    ticketedAgent: null,
-    bookedPCC: null,
-    issuesPCC: null,
-    "itinerary type": null,
-    processed: false,
-  };
-
-console.log(type);
-
-// A29PAX A29NAM (55) A29EMD(13)
+module.exports = processEmdRec;
--- utils/ProcessMIRfile.js
+++ utils/ProcessMIRfile.js
@@ -10,12 +10,13 @@ const getFinalFare = require("./getFinalFare");
 const dateTimeFormats = require("../helpers/dateTimeConverter");
 let [convertDate, convertTime] = dateTimeFormats;
 const fs = require("fs");
-const path = require("path")
+const path = require("path");
 let correctOrder = require("./correctOrder");
 const getRefundInfo = require("./getRefundData");
 const { writeToSheets, voidTicket } = require("../gglesheets/processSheets");
-const { saveToDrive } = require("../ggledrive/saveToDrive")
-const folderId = "18tAuB_F8BEV6aRYsEzhAJf21vD7H_xMP"
+const { saveToDrive } = require("../ggledrive/saveToDrive");
+const folderId = "18tAuB_F8BEV6aRYsEzhAJf21vD7H_xMP";
+const getEmdData = require("../arj/emd");
 
 function ProcessMIRfile(text) {
   let rawText = fs.readFileSync(text, "utf8");
@@ -25,7 +26,9 @@ function ProcessMIRfile(text) {
   let exchA10section = /A10.*/g;
   let A10exist = exchA10section.exec(rawText);
   let domInt = getDomInt(parsed);
-  let newName = `${type["MIR type"]}-${parsed.T50RCL}-${parsed.T50AGT}-${path.basename(text)}`
+  let newName = `${type["MIR type"]}-${parsed.T50RCL}-${
+    parsed.T50AGT
+  }-${path.basename(text)}`;
 
   A10exist !== null ? (type["MIR type"] = "EXCH") : type["MIR type"];
   // if mir file is normal tkt - ticket or exchange ticket, than provess this
@@ -50,7 +53,6 @@ function ProcessMIRfile(text) {
         return val;
       });
       try {
-
         writeToSheets(dta);
         saveToDrive(folderId, text, newName);
         // saveRefundRecord(dta);
@@ -63,15 +65,12 @@ function ProcessMIRfile(text) {
   }
   // if mir file is refund tkt
   else if (type["MIR type"] === "RFND") {
-   
     let refData = getRefundInfo(rawText, parsed, domInt, type);
     writeToSheets(refData);
     saveToDrive(folderId, text, newName);
-
   }
   // if mir file is void tkt
   else if (type["MIR type"] === "VOID") {
-    
     let paxData = getPaxSection(rawText, parsed.T50ISA);
 
     for (let tkt of paxData.passengers) {
@@ -79,7 +78,19 @@ function ProcessMIRfile(text) {
       voidTicket(tktVoid);
       saveToDrive(folderId, text, newName);
     }
-    
+  } else if (type["MIR type"] === "EMD") {
+    let emdData = getEmdData(text);
+    for (let i of emdData) {
+      let data = Object.values(i).map((val) => {
+        return val;
+      });
+      try {
+        writeToSheets(data);
+        saveToDrive(folderId, text, newName);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 }
 
